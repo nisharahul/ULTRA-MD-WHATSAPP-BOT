236c4087af81ad44e27239025e36aea8acae66e5
nisharahul <nisharahul7579@gmail.com>
2024-11-25 10:55:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '917017659124';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || 'your number';  // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '917017659124';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -19,8 +19,8 @@ for (let i = 0; i < ownerlist.length; i++) {
     global.owner.push([ownerlist[i], true]);
 }
 //
-global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.pairingNumber = process.env.BOT_NUMBER || 'your number';  // put your number here
+global.botname = process.env.BOTNAME || '👑 𝙍𝘼𝙃𝙐𝙇 𝙋𝘼𝙉𝘾𝙃𝘼𝙇 𝙑𝙄𝙎𝙃𝘼𝙒𝘼𝙆𝘼𝙍𝙈𝘼 𝘽𝙍𝘼𝙃𝙈𝘼𝙉 𝘽𝙊𝙔 👑';
+global.pairingNumber = process.env.BOT_NUMBER || 'RAHUL PANCHAL BOY';  // put your number here
 global.SESSION_ID = process.env.SESSION_ID || 'session id';  // put your session id here
 
 global.mods = []
@@ -90,6 +90,8 @@ global.dmoji = '🤭'
 global.done = '✅'
 global.error = '❌'
 global.xmoji = '🤩'
+global.typing = '💝'
+global.messages = '💋'
 
 global.multiplier = 69
 global.maxwarn = '3'
